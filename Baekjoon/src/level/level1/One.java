3d82dae8e92348109be91d6f0c4290ece7a22762
codeguma <codeguma@gmail.com>
2020-04-03 22:21:52 +0900

    백준5일

--- Baekjoon/src/level/level1/One.java
+++ Baekjoon/src/level/level1/One.java
@@ -1,4 +1,7 @@
 package level.level1;
+
+import java.util.Scanner;
+
 /*
  * level1 : 입출력과 사칙연산
  */
@@ -6,11 +9,13 @@ public class One {
 
 	public static void main(String[] args) {
 		One start = new One();
-		start.problem1();
-		start.problem2();
-		start.problem3();
-		start.problem4();
-		//왜....
+//		start.problem1();
+//		start.problem2();
+//		start.problem3();
+//		start.problem4();
+		start.problem5();
+		
+		
 	}
 	
 	public void problem1() {
@@ -54,5 +59,17 @@ public class One {
 				"|\"^\"`    |\r\n" + 
 				"||_/=\\\\__|");
 	}
-
+	public void problem5() {
+		/*두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		 * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.println("A를 입력하시오.");
+		int A = sc.nextInt();
+		System.out.println("B를 입력하시오.");
+		int B = sc.nextInt();
+		sc.close();
+		
+		System.out.println(A+B);
+	}
 }
