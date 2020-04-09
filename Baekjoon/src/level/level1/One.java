7e470787a1b38d0373f793f15b575e11546abfba
codeguma <codeguma@gmail.com>
2020-04-09 22:46:02 +0900

    no message

--- Baekjoon/src/level/level1/One.java
+++ Baekjoon/src/level/level1/One.java
@@ -17,7 +17,8 @@ public class One {
 //		start.problem1001();
 //		start.problem10998();
 //		start.problem1008();
-		start.problem10869();
+//		start.problem10869();
+		start.problem10430();
 		
 		
 	}
@@ -119,5 +120,26 @@ public class One {
 		System.out.println(A*B);
 		System.out.println(A/B);
 		System.out.println(A%B);
+	}
+	public void problem10430() {
+		/*
+		 * (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+		 * (A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+		       세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+		       
+		    첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+		    
+		  첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.     
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C) + (B%C))%C);
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
 	}
 }
