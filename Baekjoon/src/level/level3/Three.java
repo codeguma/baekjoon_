260c111264ca34bdbf2f519735dfe3bd1ee9f47c
codeguma <codeguma@gmail.com>
2020-04-20 22:37:37 +0900

    no message

--- Baekjoon/src/level/level3/Three.java
+++ Baekjoon/src/level/level3/Three.java
@@ -1,5 +1,10 @@
 package level.level3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Three {
@@ -8,7 +13,8 @@ public class Three {
 		Three start = new Three();
 //		start.problem2739();
 //		start.problem10950();
-		start.problem8393();
+//		start.problem8393();
+		start.problem15552();
 		
 	}
 	public void problem2739() {
@@ -53,5 +59,24 @@ public class Three {
 			sum += i ;
 		}
 		System.out.println(sum);
+	}
+	public void problem15552() throws NumberFormatException, IOException {
+		/*
+		 * 
+		 */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		 int T = Integer.parseInt(br.readLine().trim());
+		
+		 for (int i=0; i < T; i++) {
+	            String text = br.readLine();
+	            String[] word = text.split(" ");
+	            int A = Integer.parseInt(word[0]);
+	            int B = Integer.parseInt(word[1]);
+	            bw.write((A+B) + "\n");
+	        }
+	        
+	        bw.flush();
+	        bw.close();
 	}
 }
