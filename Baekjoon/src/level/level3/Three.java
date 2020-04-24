21eec3d9214891ec1bbb9699802e1aca676c4f2e
codeguma <codeguma@gmail.com>
2020-04-24 22:48:36 +0900

    no message

--- Baekjoon/src/level/level3/Three.java
+++ Baekjoon/src/level/level3/Three.java
@@ -17,7 +17,8 @@ public class Three {
 //		start.problem15552();
 //		start.problem2741();
 //		start.problem2742();
-		start.problem11021();
+//		start.problem11021();
+		start.problem11022();
 	}
 	public void problem2739() {
 		/*
@@ -111,5 +112,19 @@ public class Three {
 			int B = sc.nextInt();
 			System.out.println("Case #"+i+":"+(A+B));
 		}
+		sc.close();
+	}
+	public void problem11022() {
+		/*
+		 * 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =1 ; i <= T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println("Case #"+i+": "+A+" + "+B+" = "+(A+B));
+		}
+		sc.close();
 	}
 }
