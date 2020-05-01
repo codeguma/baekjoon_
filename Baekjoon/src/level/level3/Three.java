02208f2fd95a2b90bb21c77d58c95277c2510b39
codeguma <codeguma@gmail.com>
2020-05-01 23:29:17 +0900

    no message

--- Baekjoon/src/level/level3/Three.java
+++ Baekjoon/src/level/level3/Three.java
@@ -20,7 +20,8 @@ public class Three {
 //		start.problem11021();
 //		start.problem11022();
 //		start.problem2438();
-		start.problem2439();
+//		start.problem2439();
+		start.problem10871();
 	}
 	public void problem2739() {
 		/*
@@ -150,5 +151,11 @@ public class Three {
 				System.out.print("*");
 			System.out.println();
 		}
+	}
+	public void problem10871() {
+		/*
+		 * 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+		 */
+		
 	}
 }
