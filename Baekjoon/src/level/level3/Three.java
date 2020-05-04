c277ef3596fb7a9e9888bc23b38b567690416a72
codeguma <codeguma@gmail.com>
2020-05-04 21:58:16 +0900

    no message

--- Baekjoon/src/level/level3/Three.java
+++ Baekjoon/src/level/level3/Three.java
@@ -156,6 +156,21 @@ public class Three {
 		/*
 		 * 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
 		 */
+		  Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int x = sc.nextInt();
+	         
+	        int[] a = new int[n];
+	        for (int i = 0; i < a.length; ++i) {
+	            a[i] = sc.nextInt();
+	        }
+	        sc.close();
+	         
+	        for (int i = 0; i < a.length; ++i) {
+	            if (a[i] < x) {
+	                System.out.print(a[i] + " ");
+	            }
+	        }
+	    }
 		
-	}
 }
