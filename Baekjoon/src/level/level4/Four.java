c1d07e5142688b583d055bb1446377e8f3d22506
codeguma <codeguma@gmail.com>
2020-05-11 23:32:32 +0900

    no message

--- Baekjoon/src/level/level4/Four.java
+++ Baekjoon/src/level/level4/Four.java
@@ -36,6 +36,19 @@ public class Four {
 		 }
 	public void problem1110() {
 		Scanner sc = new Scanner(System.in);
+		
+	    int n = sc.nextInt();
+        sc.close();
+         
+        int number = n;
+        int count = 0;
+         
+        do {
+            number = number % 10 * 10 + (number / 10 + number % 10) % 10;
+            count++;
+        } while (n != number);
+         
+        System.out.println(count);
 	}
 
 }
