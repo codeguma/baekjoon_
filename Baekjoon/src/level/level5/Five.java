5dd25b017076b5e00ee3c32b3872efd18ace0923
codeguma <codeguma@gmail.com>
2020-05-13 22:31:02 +0900

    no message

---
+++ Baekjoon/src/level/level5/Five.java
@@ -0,0 +1,25 @@
+package level.level5;
+
+import java.util.Scanner;
+
+public class Five {
+
+	public static void main(String[] args) {
+		Five start = new Five();
+		start.problem10039();
+	}
+	public void problem10039() {
+		Scanner sc = new Scanner(System.in);
+		int sum =0;
+		int a;
+		for(int i =0 ; i < 5 ; i++) {
+			a = sc.nextInt();
+			if(a <40) a = 40;
+			
+			sum += a;
+		}
+		System.out.println(sum/5);
+		
+	}
+
+}
