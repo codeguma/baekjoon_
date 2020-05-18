363b6d3437832a52dc4df6c9a841820bd6169c63
codeguma <codeguma@gmail.com>
2020-05-18 23:36:12 +0900

    no message

--- Baekjoon/src/level/level5/Five.java
+++ Baekjoon/src/level/level5/Five.java
@@ -24,7 +24,29 @@ public class Five {
 	}
 	
 	public void problem5543() {
-		
+		Scanner in = new Scanner(System.in);
+		 
+		int burger = 2001;
+		int drink = 2001;
+ 
+		// 버거
+		for (int i = 0; i < 3; i++) {
+			int value = in.nextInt();
+			if (value < burger) {	// 최솟값 찾기
+				burger = value;
+			}
+		}
+ 
+		// 음료
+		for (int i = 0; i < 2; i++) {
+			int value = in.nextInt();
+			if (value < drink) {	// 최솟값 찾기
+				drink = value;
+			}
+		}
+ 
+		System.out.println(burger + drink - 50);
+	}
 		
 		
 	}
