956d245f4b1d32bab9575c87bf16339856858bfc
codeguma <codeguma@gmail.com>
2020-07-01 23:43:23 +0900

    no message

--- Baekjoon/src/level/level5/Five.java
+++ Baekjoon/src/level/level5/Five.java
@@ -97,11 +97,40 @@ public class Five {
 	}
 	public void problem2446() {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
+		int n = sc.nextInt();
 		
-		for(int i = a; i <= a && i > 0; i--) {
-			for(int j = 1 ; j < a*2 ; j++) {
+		for(int i = 0; i <n; i++) {
+			if(i == 0) {
+			}
+			else {
+				for(int a =0; a < i; a++ ) System.out.print(" ");
+			}
+			for(int j = 0 ; j < 2*n-(2*i+1) ; j++) {
 				System.out.print("*");
+			}
+			if(i == 0) {
+			}
+			else {
+				for(int a =0; a < i; a++ ) System.out.print(" ");
+			}
+			if(i == n-1) {
+			}else {
+			System.out.println();
+			}
+		}
+		for(int i = n; i > 0; i--) {
+			if(i == n) {
+			}
+			else {
+				for(int a =0; a < i; a++ ) System.out.print(" ");
+			}
+			for(int j = 2*n-(2*i+1) ; j >1  ; j--) {
+				System.out.print("*");
+			}
+			if(i == n) {
+			}
+			else {
+				for(int a =0; a < i; a++ ) System.out.print(" ");
 			}
 			System.out.println();
 		}
